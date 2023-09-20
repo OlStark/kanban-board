23d41507c8aba759cea947cdfa90b46a793e2ada
OLStark <78405223+starkov-sys@users.noreply.github.com>
2023-09-20 11:05:21 +0300

    some changes

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,18 +1,15 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Board from '../Board/Board';
-import TaskDetails from '../Task-details/details';
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Board from "../Board/Board";
+import TaskDetails from "../Task-details/details";
 
 const Main = (props) => {
+  return (
+    <Routes>
+      <Route exact path={"/"} element={<Board {...props} />} />
+      <Route path={"/tasks/:taskID"} element={<TaskDetails {...props} />} />
+    </Routes>
+  );
+};
 
-    return(
-        <Routes>
-            <Route exact path={'/'} element={<Board{...props}/>}/>
-            <Route path={'/tasks/:taskID'} element={<TaskDetails{...props}/>}/>
-        </Routes>
-        
-    )
-    }
-
-export default Main ;
+export default Main;
